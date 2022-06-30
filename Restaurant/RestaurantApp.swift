46d4a600121dc281492eb36bc1f4bd432a0f1744
budanovDmitriy <lllytnick1@gmail.com>
2022-06-30 13:14:58 +0300

    first commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by Dmitriy Budanov on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
